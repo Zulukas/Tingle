a7edcc4cf0a676654e1af0c18ffd8ae4be5737ac
Zulukas <1996nissan200sx@gmail.com>
2014-12-01 09:47:29 -0700

    12/1/2014 push: Pushing what we have so far!  Basic GUI right now with the data and file handling.  Ready for basic unit testing.

--- Tingle/src/tingleGUI/Tingle.java
+++ Tingle/src/tingleGUI/Tingle.java
@@ -5,50 +5,91 @@
  */
 package tingleGUI;
 
-import java.awt.Graphics;
 import tingleData.TingleDataManager;
 import java.io.File;
-import java.util.List;
-import tingleData.Tile;
+import javax.swing.JScrollPane;
 
 /**
  *
  * @author Kevin Andres
  */
-public class Tingle {
+public class Tingle extends javax.swing.JFrame {
     private MapGridGUI myMapGridGUI;        //The part of the GUI containing the map
     private PropertiesBox myPropertiesBox;  //The part of the GUI relating to which properties a tile has
     private TileSelector myTileSelector;    //The part of the GUI which contains placeable tiles
     private TingleDataManager myTingleDataManager;
 
+    //GUI OBJECTS
+    private JScrollPane mapView;
+    
     public Tingle(String filename) {
         myTingleDataManager = new TingleDataManager(new File(filename));        
+        initComponents();
     }
     
     
     public void start() {
         myTingleDataManager.loadFile();
-        
-        List<Tile> myTiles = myTingleDataManager.getMapTiles();
-        System.out.println(myTiles);
-        
-        //myTileSelector = new TileSelector();
-        //myTileSelector.paintComponent();
-        
-        //GUI will go in here
-        
+        initComponents();    
     }
     
-    public void end() {
-        //Do we need a stop function?
+    @SuppressWarnings("unchecked")
+    private void initComponents() {
+        mapView = new javax.swing.JScrollPane();
+        
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(mapView, javax.swing.GroupLayout.DEFAULT_SIZE, 780, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(mapView, javax.swing.GroupLayout.PREFERRED_SIZE, 350, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(239, Short.MAX_VALUE))
+        );
+        
+        pack();
     }
     
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Tingle tingle = new Tingle("src\\resources\\testLoad.tingle");
-        tingle.start();
-        tingle.end();
-    }    
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Tingle.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Tingle.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Tingle.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Tingle.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Tingle("src\\resources\\testLoad.tingle").setVisible(true);
+                
+            }
+        });
+    }
 }
